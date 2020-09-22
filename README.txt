c9803def7d643b823a584e487bca9122cc5d6802
Damian Alarcon <damian@nikai.cl>
2020-09-22 09:55:22 -0300

    Initial release
    Version 1.0

---
+++ README.txt
@@ -0,0 +1,13 @@
+#A filter plugin to import content from the Studietube
+
+##Description
+Publish content from Studietube
+
+## Installation
+- Open your Site administration
+- Select Plugins -> Install plugins and upload the zip file containing this plugin, alternatively you can add the content of this plugin directly to /lib/editor/atto/plugins and open moodle on your browser to complete the installation process
+
+## Usage
+- To use the plugin, simply open any Atto editor and you should see the Studietube button on the toolbar
+- Click on the Studietube button and login if you're asked
+- Select any of the media stored on Studietube to add it directly into the editor
---
+++ filter.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Filter plugin for Lærit.dk
+ *
+ * @package    filter_studietube
+ * @copyright  2020 Damian Alarcon
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ */
+global $CFG;
+require_once($CFG->libdir . '/filelib.php');
+defined('MOODLE_INTERNAL') || die();
+class filter_studietube extends moodle_text_filter
+{
+    /**
+     * Moodle text filter
+     *
+     * @param string $text 
+     * @param array() options
+     * @return string
+     */
+    public function filter($text, array $options = array()) {
+        if (!is_string($text) || empty($text) || 
+           (!$successmatch = preg_match_all("/<img(?:.+?)class=\"studietube\"(?:.+?)alt=\"([A-Za-z0-9]+)\"/s", $text, $matches))) {
+            return $text;
+        }
+
+        foreach($matches[1] as $match)
+        {
+            $vkey = filter_var($match, FILTER_SANITIZE_STRING);
+
+            if(empty($vkey))
+            {
+               continue; 
+            }
+
+            $text = preg_replace('/<img(?:.+?)class="studietube"(?:.+?)alt="'.$vkey.'"(?:.+?)>/s', '<p style="width:100%;height:0;position:relative;padding-bottom:56.25%;"><iframe src="\/\/www.studietube.dk\/e\/'.$vkey.'\/0?nopanel=tru" style="width:100%;height:100%;position:absolute;top:0;left:0;" allowfullscreen webkitallowfullscreen mozAllowFullScreen frameborder="0" allow="encrypted-media"></iframe></p>', $text);
+        }
+        
+        return $text;
+    }
+}
